a1c1a08e38fc777d1a3534ecfd09b2b66a2853d5
raiyanbinmohsin <raiyanbinmohsinshishir@gmail.com>
2021-03-23 00:02:00 +0600

    sdm660-common: props: Rearrange all the properties This commit is necessary to set all the properties according to their correct places. Also, this fixes Netflix crash during full HD Playback. Conflicts:        common_prop.mk Conflicts:        product.prop Conflicts:        sdm660.mk
    Co-authored-by: chrisw444 <wandersonrodriguesf1@gmail.com>
    Co-authored-by: etahamad <etahamad@icloud.com>

--- sdm660.mk
+++ sdm660.mk
@@ -47,11 +47,13 @@ COMMON_PATH := device/xiaomi/sdm660-common
 # Inherit Dirac
 $(call inherit-product-if-exists, vendor/xiaomi/dirac/dirac.mk)
 
-# Inherit properties
-$(call inherit-product, $(COMMON_PATH)/common_prop.mk)
+# Vendor properties
+-include $(COMMON_PATH)/vendor_prop.mk
 
 # Inherit properties ($PATH_PROP)
+TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
 TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
+TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
 TARGET_SYSTEM_EXT_PROP += $(COMMON_PATH)/system_ext.prop
 
 # A/B
