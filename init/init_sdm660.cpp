6c7754fd45eb7b68e3445f2ccb5b7081309c0681
xyzuan <i.xyzuan@gmail.com>
2021-04-02 21:12:27 +0700

    lavender: cleanup init_sdm660.cpp

--- init/init_sdm660.cpp
+++ init/init_sdm660.cpp
@@ -1,6 +1,6 @@
 /*
    Copyright (c) 2016, The CyanogenMod Project
-   Copyright (c) 2019-2020, The LineageOS Project
+   Copyright (c) 2019, The LineageOS Project
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are
    met:
@@ -39,10 +39,11 @@
 #include <sys/_system_properties.h>
 
 #include "vendor_init.h"
-#include "property_service.h"
 
 using android::base::GetProperty;
+using android::base::SetProperty;
 using android::base::ReadFileToString;
+using android::base::Trim;
 
 char const *heapstartsize;
 char const *heapgrowthlimit;
@@ -84,104 +85,27 @@ void check_device()
     }
 }
 
-void property_override(char const prop[], char const value[], bool add = true)
+void property_override(char const prop[], char const value[])
 {
-    auto pi = (prop_info *) __system_property_find(prop);
+    prop_info *pi;
 
-    if (pi != nullptr) {
+    pi = (prop_info*) __system_property_find(prop);
+    if (pi)
         __system_property_update(pi, value, strlen(value));
-    } else if (add) {
+    else
         __system_property_add(prop, strlen(prop), value, strlen(value));
-    }
 }
 
-void property_override_dual(char const system_prop[],
-        char const vendor_prop[], char const value[])
+void property_override_triple(char const product_prop[], char const system_prop[], char const vendor_prop[],
+    char const value[])
 {
+    property_override(product_prop, value);
     property_override(system_prop, value);
     property_override(vendor_prop, value);
 }
 
-void property_override_multifp(char const buildfp[], char const systemfp[],
-        char const bootimagefp[], char const vendorfp[], char const value[]) {
-    property_override(buildfp, value);
-    property_override(systemfp, value);
-    property_override(bootimagefp, value);
-    property_override(vendorfp, value);
-}
-
-void vendor_load_persist_properties()
-{
-    std::string product = GetProperty("ro.product.vendor.device", "");
-    if (product.find("clover") != std::string::npos) {
-
-    std::string hw_device;
-
-    char const *hw_id_file = "/sys/devices/platform/HardwareInfo/hw_id";
-
-    ReadFileToString(hw_id_file, &hw_device);
-    if (hw_device.find("D9P") != std::string::npos) {
-        property_override("persist.sys.fp.vendor", "fpc");
-        property_override("ro.board.variant", "d9p");
-        property_override("vendor.display.lcd_density", "265");
-        property_override_dual("ro.product.model", "ro.vendor.product.model", "MI PAD 4 PLUS");
-
-        property_override ("persist.vendor.audio.calfile0","/vendor/etc/acdbdata/QRD/sdm660-snd-card-d9p/QRD_D9P_Bluetooth_cal.acdb");
-        property_override ("persist.vendor.audio.calfile1","/vendor/etc/acdbdata/QRD/sdm660-snd-card-d9p/QRD_D9P_General_cal.acdb");
-        property_override ("persist.vendor.audio.calfile2","/vendor/etc/acdbdata/QRD/sdm660-snd-card-d9p/QRD_D9P_Global_cal.acdb");
-        property_override ("persist.vendor.audio.calfile3","/vendor/etc/acdbdata/QRD/sdm660-snd-card-d9p/QRD_D9P_Handset_cal.acdb");
-        property_override ("persist.vendor.audio.calfile4","/vendor/etc/acdbdata/QRD/sdm660-snd-card-d9p/QRD_D9P_Hdmi_cal.acdb");
-        property_override ("persist.vendor.audio.calfile5","/vendor/etc/acdbdata/QRD/sdm660-snd-card-d9p/QRD_D9P_Headset_cal.acdb");
-        property_override ("persist.vendor.audio.calfile6","/vendor/etc/acdbdata/QRD/sdm660-snd-card-d9p/QRD_D9P_Speaker_cal.acdb");
-        property_override ("persist.vendor.audio.calfile7","/vendor/etc/acdbdata/QRD/sdm660-snd-card-d9p/QRD_D9P_workspaceFile.qwsp");
-        property_override ("persist.vendor.audio.calfile8","/vendor/etc/acdbdata/adsp_avs_config.acdb");
-
-    } else {
-        property_override("persist.sys.fp.vendor", "none");
-        property_override("ro.board.variant", "d9");
-        property_override("vendor.display.lcd_density", "320");
-        property_override_dual("ro.product.model", "ro.vendor.product.model", "MI PAD 4");
-
-        property_override ("persist.vendor.audio.calfile0","/vendor/etc/acdbdata/QRD/sdm660-snd-card-skush/QRD_SKUSH_Bluetooth_cal.acdb");
-        property_override ("persist.vendor.audio.calfile1","/vendor/etc/acdbdata/QRD/sdm660-snd-card-skush/QRD_SKUSH_General_cal.acdb");
-        property_override ("persist.vendor.audio.calfile2","/vendor/etc/acdbdata/QRD/sdm660-snd-card-skush/QRD_SKUSH_Global_cal.acdb");
-        property_override ("persist.vendor.audio.calfile3","/vendor/etc/acdbdata/QRD/sdm660-snd-card-skush/QRD_SKUSH_Handset_cal.acdb");
-        property_override ("persist.vendor.audio.calfile4","/vendor/etc/acdbdata/QRD/sdm660-snd-card-skush/QRD_SKUSH_Hdmi_cal.acdb");
-        property_override ("persist.vendor.audio.calfile5","/vendor/etc/acdbdata/QRD/sdm660-snd-card-skush/QRD_SKUSH_Headset_cal.acdb");
-        property_override ("persist.vendor.audio.calfile6","/vendor/etc/acdbdata/QRD/sdm660-snd-card-skush/QRD_SKUSH_Speaker_cal.acdb");
-        property_override ("persist.vendor.audio.calfile7","/vendor/etc/acdbdata/QRD/sdm660-snd-card-skush/QRD_SKUSH_workspaceFile.qwsp");
-        property_override ("persist.vendor.audio.calfile8","/vendor/etc/acdbdata/adsp_avs_config.acdb");
-        
-    }
-  }
-}
 void vendor_load_properties()
 {
-    check_device();
-    
-    property_override("dalvik.vm.heapstartsize", heapstartsize);
-    property_override("dalvik.vm.heapgrowthlimit", heapgrowthlimit);
-    property_override("dalvik.vm.heapsize", heapsize);
-    property_override("dalvik.vm.heaptargetutilization", heaptargetutilization);
-    property_override("dalvik.vm.heapminfree", heapminfree);
-    property_override("dalvik.vm.heapmaxfree", heapmaxfree);
-
-   std::string product = GetProperty("ro.product.vendor.device", "");	
-   if (product.find("whyred") != std::string::npos)
-   {
-  	std::string region = GetProperty("ro.boot.hwc", "");
-
-    if (region.find("CN") != std::string::npos || region.find("Global") != std::string::npos || region.find("GLOBAL") != std::string::npos)
-	{
-        property_override_dual("ro.product.model", "ro.vendor.product.model", "Redmi Note 5");
-        property_override_dual("ro.product.odm.model", "ro.product.system.model", "Redmi Note 5");
-        property_override_dual("ro.product.vendor.model", "persist.vendor.camera.exif.model", "Redmi Note 5");
-	}
-	else
-	{
-        property_override_dual("ro.product.model", "ro.vendor.product.model", "Redmi Note 5 Pro");
-        property_override_dual("ro.product.odm.model", "ro.product.system.model", "Redmi Note 5 Pro");
-        property_override_dual("ro.product.vendor.model", "persist.vendor.camera.exif.model", "Redmi Note 5 Pro");
-	}
-  }
+    property_override("ro.build.description", "redfin-user 11 RQ2A.210305.006 7119741 release-keys");
+    property_override_triple("ro.build.fingerprint", "ro.system.build.fingerprint", "ro.vendor.build.fingerprint", "google/redfin/redfin:11/RQ2A.210305.006/7119741:user/release-keys");
 }
