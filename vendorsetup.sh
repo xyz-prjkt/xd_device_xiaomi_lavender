3185a89fcdb0888469df6d70179734d93217eaee
Jody Yuantoro <i.xyzuan@gmail.com>
2021-04-04 12:24:41 +0700

    lavender: implement AOSP Telephony

---
+++ vendorsetup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Copyright 2021 a xyzprjkt property
+
+ROOTDIR=$(realpath .)
+
+aosptel() {
+echo Patching AOSP Telephony implementation . . .
+    rm -rf frameworks/opt/telephony
+    rm -rf frameworks/opt/net/ims
+    rm -rf packages/providers/TelephonyProvider
+    rm -rf packages/services/Telephony
+    git clone https://github.com/xdroid-AOSP/frameworks_opt_telephony frameworks/opt/telephony -b eleven-aosp
+    git clone https://github.com/xdroid-AOSP/frameworks_opt_net_ims frameworks/opt/net/ims -b eleven-aosp
+    git clone https://github.com/xdroid-AOSP/packages_providers_TelephonyProvider packages/providers/TelephonyProvider -b eleven-aosp
+    git clone https://github.com/xdroid-AOSP/packages_services_Telephony packages/services/Telephony -b eleven-aosp
+}
+
+aospfwtel() {
+    cd frameworks/base
+    git fetch xdroid
+    git cherry-pick da3c7e1
+    cd ../../packages/apps/Settings
+    git fetch xdroid
+    git cherry-pick e83d6ed
+}
+
+aosptel
+aospfwtel
+cd $ROOTDIR
+
+echo "Patch Successfully !"
+    
