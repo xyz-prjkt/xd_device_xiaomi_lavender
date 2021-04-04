c1aa99cdd359cff46215e42a9ae887c33d37cb64
Jody Yuantoro <i.xyzuan@gmail.com>
2021-04-04 13:38:09 +0700

    lavender: track AOSP STK.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -10,10 +10,12 @@ echo Patching AOSP Telephony implementation . . .
     rm -rf frameworks/opt/net/ims
     rm -rf packages/providers/TelephonyProvider
     rm -rf packages/services/Telephony
+    rm -rf packages/apps/Stk
     git clone https://github.com/xdroid-AOSP/frameworks_opt_telephony frameworks/opt/telephony -b eleven-aosp
     git clone https://github.com/xdroid-AOSP/frameworks_opt_net_ims frameworks/opt/net/ims -b eleven-aosp
     git clone https://github.com/xdroid-AOSP/packages_providers_TelephonyProvider packages/providers/TelephonyProvider -b eleven-aosp
     git clone https://github.com/xdroid-AOSP/packages_services_Telephony packages/services/Telephony -b eleven-aosp
+    git clone https://github.com/xdroid-AOSP/packages_apps_Stk packages/apps/Stk -b eleven-aosp
 }
 
 aospfwtel() {
