6e108b43b79fca18b59ce0975bd7da3b8651b67c
Kuba Wojciechowski <nullbytepl@gmail.com>
2021-05-05 19:34:08 +0800

    sdm660-common: Disable KPTI feature
    It can also help increase performance by a lot in some scenarios.
    
    Signed-off-by: Kuba Wojciechowski <nullbytepl@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -152,6 +152,7 @@ BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 androidboot.console=ttyMSM0 ear
 BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_CMDLINE += printk.devkmsg=on
 BOARD_KERNEL_CMDLINE += usbcore.autosuspend=7
+BOARD_KERNEL_CMDLINE += kpti=off
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
