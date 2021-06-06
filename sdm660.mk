4872ede6a5c9d39620d616a6b1fe4912693dbbdd
OdSazib <odsazib@gmail.com>
2021-06-06 19:09:23 +0600

    sdm660-common: Update RIL from curtana V12.0.1.7.RJWMIXM - Update android.hardware.secure_element to 1.2 - Update android.hardware.radio to 1.5 - Update vendor.qti.hardware.radio.uim_remote_client to 1.2 - Downgrade vendor.qti.hardware.radio.lpa to 1.0 - Add seccomp_policy for imsrtp - Remove duplicate vendor.qti.hardware.radio.qtiradio entry - Remove libshim for libdpmframework - Adjust props with new RIL blobs
    Signed-off-by: OdSazib <odsazib@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -493,10 +493,10 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
-    android.hardware.radio@1.4 \
+    android.hardware.radio@1.5 \
     android.hardware.radio@1.2 \
     android.hardware.radio.config@1.1 \
-    android.hardware.secure_element@1.0 \
+    android.hardware.secure_element@1.2 \
     librmnetctl \
     libxml2 \
     libprotobuf-cpp-full \
@@ -513,6 +513,7 @@ PRODUCT_BOOT_JARS += \
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/seccomp/imsrtp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/imsrtp.policy \
     $(COMMON_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
     $(COMMON_PATH)/seccomp/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
 
@@ -539,10 +540,6 @@ PRODUCT_SOONG_NAMESPACES += \
     $(COMMON_PATH) \
     hardware/google/interfaces \
     hardware/google/pixel
-
-# Shims
-PRODUCT_PACKAGES += \
-    libcutils_shim
 
 # Tetheroffload
 PRODUCT_PACKAGES += \
