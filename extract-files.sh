0381f4649cf36ab04198de8cc68552917ae306b8
OdSazib <odsazib@gmail.com>
2021-06-18 19:19:31 +0600

    sdm660-common: Patch some wfd blobs

--- extract-files.sh
+++ extract-files.sh
@@ -74,10 +74,14 @@ fi
 function blob_fixup() {
     case "${1}" in
 
+    lib64/libwfdnative.so)
+        "${PATCHELF}" --remove-needed "android.hidl.base@1.0.so" "${2}"
+        ;;
+
     vendor/bin/mlipayd@1.1)
         "${PATCHELF}" --remove-needed vendor.xiaomi.hardware.mtdservice@1.0.so "${2}"
         ;;
-    
+
     vendor/lib64/libmlipay.so | vendor/lib64/libmlipay@1.1.so)
         "${PATCHELF}" --remove-needed vendor.xiaomi.hardware.mtdservice@1.0.so "${2}"
         sed -i "s|/system/etc/firmware|/vendor/firmware\x0\x0\x0\x0|g" "${2}"
