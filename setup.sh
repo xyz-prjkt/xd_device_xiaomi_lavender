4e3d257857c97fab0a3013fcd518e778ac350397
Jody Yuantoro <i.xyzuan@gmail.com>
2021-06-29 04:50:17 +0700

    lavender: use xRageTC Clang

--- setup.sh
+++ setup.sh
@@ -3,3 +3,4 @@ git clone https://github.com/xyz-prjkt/xbore_strmbreakr_lavender ../../../kernel
 git clone https://github.com/xyz-prjkt/hw_media ../../../hardware/qcom-caf/msm8998/media -b 11-LA.UM.9.6.2.r1-03600-89xx.0
 git clone https://github.com/xyz-prjkt/hw_audio ../../../hardware/qcom-caf/msm8998/audio -b 11-LA.UM.9.6.2.r1-03600-89xx.0
 git clone https://github.com/xyz-prjkt/hw_display ../../../hardware/qcom-caf/msm8998/display -b 11-LA.UM.9.6.2.r1-03600-89xx.0
+git clone https://github.com/xyz-prjkt/xRageTC-clang ../../../prebuilts/clang/host/linux-x86/clang-xRage --depth=1
