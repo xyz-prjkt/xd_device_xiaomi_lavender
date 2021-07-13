0857552a5781222ecb218c00ba0931f2d7a57fcb
Jody Yuantoro <xyzuannihboss@gmail.com>
2021-07-13 14:38:22 +0700

    lavender: setup.sh: cleanup.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,2 @@
 git clone https://github.com/xyz-prjkt/xdroid_vendor_lavender ../../../vendor/xiaomi/lavender -b eleven-caf
 git clone https://github.com/xyz-prjkt/xbore_strmbreakr_lavender ../../../kernel/xiaomi/lavender -b eleven-caf_eas
-git clone https://github.com/xyz-prjkt/hw_media ../../../hardware/qcom-caf/msm8998/media -b 11-LA.UM.9.6.2.r1-03600-89xx.0
-git clone https://github.com/xyz-prjkt/hw_audio ../../../hardware/qcom-caf/msm8998/audio -b 11-LA.UM.9.6.2.r1-03600-89xx.0
-git clone https://github.com/xyz-prjkt/hw_display ../../../hardware/qcom-caf/msm8998/display -b 11-LA.UM.9.6.2.r1-03600-89xx.0
-git clone https://github.com/xyz-prjkt/xRageTC-clang ../../../prebuilts/clang/host/linux-x86/clang-xRage --depth=1
