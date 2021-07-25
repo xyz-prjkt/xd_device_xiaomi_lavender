6bab253f69b48f6077d23762bfdcd3541430d76d
ghostrider-reborn <adithya.r02@outlook.com>
2021-07-25 13:25:15 +0800

    sdm660-common: Introduce kernelspace battery saver
     * Needs Kernel side support
     * This activates kernelspace battery saver via powerhal whenever
       battery saver is enabled in userspace, thereby lowering power
       consumption at kernel-level by disabling boosts and such
    
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>
    Change-Id: I2623503db27d7518de519bcaa3f5af6ab83879d0

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -79,6 +79,9 @@ BOARD_SUPPORTS_SOUND_TRIGGER := true
 USE_CUSTOM_AUDIO_POLICY := 1
 USE_XML_AUDIO_POLICY_CONF := 1
 
+# Battery Saver
+TARGET_POWERHAL_MODE_EXT := $(COMMON_PATH)/power-libperfmgr/power/power-mode.cpp
+
 # Bluetooth
 BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_QCOM := true
