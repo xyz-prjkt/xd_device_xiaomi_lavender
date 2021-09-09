b19b118ec15a369c07094461abecfa1d18bd72a5
xyzuan <xyzuannihboss@gmail.com>
2021-09-09 12:24:13 +0700

    lavender: Switch to Thermal 1.0 & ThermalControler

--- device.mk
+++ device.mk
@@ -553,7 +553,13 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.mock
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service \
+    thermal.sdm660
+
+# Thermal Controller
+PRODUCT_PACKAGES += \
+    ThermalController
 
 # Thermal Conf
 PRODUCT_COPY_FILES += \
