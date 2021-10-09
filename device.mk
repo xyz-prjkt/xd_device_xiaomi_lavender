e5ea77472a287341f652332b04b1431c92bbe0ae
xyzuan <xyzuannihboss@gmail.com>
2021-10-09 01:02:04 +0000

    lavender: uprev hardware.audio.* to v6.0

--- device.mk
+++ device.mk
@@ -42,10 +42,10 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/configs/props/vendor.prop
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@4.0-impl \
-    android.hardware.audio.effect@4.0-impl \
-    android.hardware.audio.effect@2.0-service \
+    android.hardware.audio@6.0-impl:32 \
+    android.hardware.audio.effect@6.0-impl:32 \
     android.hardware.audio.service \
+    android.hardware.soundtrigger@2.1-impl:32 \
     audio.a2dp.default \
     audio.primary.sdm660 \
     audio.r_submix.default \
