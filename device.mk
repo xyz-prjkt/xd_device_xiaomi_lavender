bbe80253550e208ac8eaab26ee8896e8f45d5e3b
xyzuan <xyzuannihboss@gmail.com>
2021-10-11 15:55:03 +0000

    lavender: Build `android.hidl.base@1.0_vendor`

--- device.mk
+++ device.mk
@@ -236,7 +236,7 @@ PRODUCT_PACKAGES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.base@1.0.vendor \
+    android.hidl.base@1.0_vendor \
     libhidltransport \
     libhidltransport.vendor \
     libhwbinder \
