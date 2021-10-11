9f2a05044d77e114b8f61aa91caa093ab37641b6
xyzuan <xyzuannihboss@gmail.com>
2021-10-11 11:06:42 +0000

    lavender: fp: Update to `Redfin 12 SPB5.210812.002`

--- xdroid_lavender.mk
+++ xdroid_lavender.mk
@@ -43,10 +43,10 @@ XDROID_UI_BLUR := true
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="lavender" \
     PRODUCT_NAME="lavender" \
-    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210905.001 7511028 release-keys"
+    PRIVATE_BUILD_DESC="redfin-user 12 SPB5.210812.002 7671067 release-keys"
 
 # Build Fingerprint
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210905.001/7511028:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:12/SPB5.210812.002/7671067:user/release-keys"
 
 # Device identifier
 PRODUCT_NAME := xdroid_lavender
