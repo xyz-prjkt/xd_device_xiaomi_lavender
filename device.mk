1bc07b12313c6f085579354d3a0bb620e6a27966
xyzuan <xyzuannihboss@gmail.com>
2021-10-13 12:43:30 +0000

    lavender: Drop `android.hardware.atrace@1.0-service` HAL
    We didnt need that, spaming log service tagged as fatal error.
    
    @xyzuan
    Drop atrace service in vndk 30 too

--- device.mk
+++ device.mk
@@ -94,10 +94,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.authsecret@1.0-service
 
-# ATRACE_HAL
-PRODUCT_PACKAGES += \
-    android.hardware.atrace@1.0-service
-
 # Biometrics
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.xiaomi_sdm660
