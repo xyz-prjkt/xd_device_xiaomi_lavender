4a2a3a1b19b53031289225785714ff1665ba9275
Jody Yuantoro <xyzuannihboss@gmail.com>
2021-11-02 17:07:50 +0700

    lavender: Create vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/xyz-prjkt/xd_vendor_xiaomi_lavender ../../../vendor/xiaomi/lavender -b twelve
+git clone https://github.com/xyz-prjkt/xbore_strmbreakr_lavender ../../../kernel/xiaomi/lavender -b v4.4.xxx-bpf
+git clone https://github.com/xdroid-CAF/xd_hardware_qcom_display ../../../hardware/qcom/display -b twelve_caf_msm8998
+git clone https://github.com/xdroid-CAF/xd_hardware_qcom_media ../../../hardware/qcom/media -b twelve_caf_msm8998
+git clone https://github.com/xdroid-CAF/xd_hardware_qcom_audio ../../../vendor/qcom/opensource/audio-hal/primary-hal -b twelve_caf_msm8998
