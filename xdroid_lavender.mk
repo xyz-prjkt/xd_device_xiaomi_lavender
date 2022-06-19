4408fdf00b5ce8f6df58349bdffc6f5647e83a52
Jody Yuantoro <xyzuannihboss@gmail.com>
2022-06-19 09:36:37 +0700

    lavender: Update xdroid_lavender.mk

--- xdroid_lavender.mk
+++ xdroid_lavender.mk
@@ -24,7 +24,6 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/xiaomi/lavender/device.mk)
@@ -35,18 +34,19 @@ $(call inherit-product, vendor/xiaomi/lavender/vendor-sdm660.mk)
 
 # Inherit some common xdroid stuff.
 $(call inherit-product, vendor/xdroid/config/common.mk)
-XDROID_BUILD_TYPE := OBT-4.19
-XDROID_BOOT_DARK := true
-XDROID_UI_BLUR := true
+XDROID_BOOT := 1080
+XDROID_MAINTAINER := xyzuan
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
+TARGET_INCLUDE_STOCK_ARCORE := false
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_SUPPORTS_QUICK_TAP := true
 
-# Build Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="lavender" \
     PRODUCT_NAME="lavender" \
-    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210805.001.A1 7474174 release-keys"
+    PRIVATE_BUILD_DESC="lavender-user 9 PKQ1.180904.001 V11.0.5.0.PFGMIXM release-keys"
 
-# Build Fingerprint
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys"
+BUILD_FINGERPRINT :="google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys"
 
 # Device identifier
 PRODUCT_NAME := xdroid_lavender
